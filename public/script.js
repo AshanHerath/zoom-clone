04348dca3fc48133cf29028f6192742f261639fa
sheronfdo <jamithsheron5@gmail.com>
2022-10-01 20:41:48 +0530

    latest with bug

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
+const { Socket } = require('socketio');
 
 const videoGrid = document.getElementById('video-grid');
-console.log(videoGrid);
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
@@ -12,6 +12,8 @@ navigator.mediaDevices.getUserMedia({
   myVideoStream = stream;
   addVideoStream(myVideo, stream);
 })
+
+Socket.emit('join-room');
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
