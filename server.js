f865b2263a5bb3818bf544a1880c3df1cf8bd2dc
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2022-10-01 16:31:06 +0530

    update script.js , server.js , room.ejs
    view our own video

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const { v4: uuid4 } = require('uuid');
-
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 app.get('/', (req, res)=> {
   res.redirect(`/${uuid4()}`);
 })
@@ -13,5 +12,5 @@ app.get('/:room', (req, res)=> {
   res.render('room', {roomId: req.params.room })
 })
 
-server.listen(3030);
+server.listen(3031);
 
